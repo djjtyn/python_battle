cc2833369abd76b4449d77700e51f87bfbc08aaa
Dave <mymac@Macbook.local>
2020-06-28 16:59:29 +0100

    additional functions added to player class

--- classes/game.py
+++ classes/game.py
@@ -8,9 +8,9 @@ class bcolors:
     FAIL = '\033[91m'
     ENDC = '\033[0m'
     BOLD = '\033[1m'
-    UNDERLINE = '\033[4m
+    UNDERLINE = '\033[4m'
 
-class person:
+class Person:
     def __init__(self, hp, mp, attack, defense, magic):
         self.max_hp = hp
         self.hp = hp
@@ -24,3 +24,50 @@ class person:
 
     def generate_damage(self):
         return random.randrange(self.atklow, self.atkhigh)
+
+    def generate_spell_damage(self, i):
+        mgl = self.magic[i]['dmg'] - 5
+        mgh = self.magic[i]['dmg'] + 5
+        return random.randrange(mgl, mgh)
+
+    def take_damage(self, dmg):
+        self.hp -= dmg
+        if self.hp < 0:
+            self.hp = 0
+        return self.hp
+
+    def get_hp(self):
+        return self.hp
+
+    def get_max_hp(self):
+        return self.max_hp
+
+    def get_mp(self):
+        return self.mp
+
+    def get_max_mp(self):
+        return self.max_mp
+
+    def reduce_mp(self, cost):
+        self.mp -= cost
+
+    def get_spell_name(self, i):
+        return self.magic[i]['name']
+
+    def get_spell_mp_cost(self, i):
+        return self.magic[i]['cost']
+
+    def choose_action(self):
+        i = 1
+        print('Actions')
+        for item in self.actions:
+            print(str(i) + ':', item)
+            i += 1
+
+    def choose_magic(self):
+        i = 1
+        print('Magic')
+        for spell in self.magic:
+            print(str(i) + ':', spell['name'], "(cost:", str(spell['mp']) + ")")
+            i += 1
+
