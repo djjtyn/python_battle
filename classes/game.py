5bd0d39e104478745c5182f7592ec8126e93838d
Dave <mymac@Macbook.local>
2020-06-29 14:39:59 +0100

    magic now has a class

--- classes/game.py
+++ classes/game.py
@@ -1,4 +1,5 @@
 import random
+from .magic import Spell
 
 class bcolors:
     HEADER = '\033[95m'
@@ -25,16 +26,16 @@ class Person:
     def generate_damage(self):
         return random.randrange(self.atklow, self.atkhigh)
 
-    def generate_spell_damage(self, i):
-        mgl = self.magic[i]['dmg'] - 5
-        mgh = self.magic[i]['dmg'] + 5
-        return random.randrange(mgl, mgh)
-
     def take_damage(self, dmg):
         self.hp -= dmg
         if self.hp < 0:
             self.hp = 0
         return self.hp
+
+    def heal(self, dmg):
+        self.hp += dmg
+        if self.hp > self.max_hp:
+            self.hp = self.max_hp
 
     def get_hp(self):
         return self.hp
@@ -51,12 +52,6 @@ class Person:
     def reduce_mp(self, cost):
         self.mp -= cost
 
-    def get_spell_name(self, i):
-        return self.magic[i]['name']
-
-    def get_spell_mp_cost(self, i):
-        return self.magic[i]['cost']
-
     def choose_action(self):
         i = 1
         print(bcolors.OKBLUE + bcolors.BOLD + 'Actions' + bcolors.ENDC)
@@ -68,6 +63,6 @@ class Person:
         i = 1
         print(bcolors.OKBLUE + bcolors.BOLD + 'Magic' + bcolors.ENDC)
         for spell in self.magic:
-            print(str(i) + ':', spell['name'], "(cost:", str(spell['cost']) + ")")
+            print(str(i) + ':', spell.name, "(cost:", str(spell.cost) + ")")
             i += 1
 
