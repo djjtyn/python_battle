5ccd0253d9e3a050131d8c4dcd941549512950c0
Dave <mymac@Macbook.local>
2020-06-29 13:07:57 +0100

    magic working

--- main.py
+++ main.py
@@ -1,12 +1,66 @@
 from classes.game import Person, bcolors
 
-magic = [{'name': 'Fire', 'cost': 10, 'dmg': 60},
-         {'name': 'Thunder', 'cost': 10, 'dmg': 80},
-         {'name': 'Blizzard', 'cost': 10, 'dmg': 60}]
+magic = [{'name': 'Fire', 'cost': 10, 'dmg': 100},
+         {'name': 'Thunder', 'cost': 12, 'dmg': 124},
+         {'name': 'Blizzard', 'cost': 10, 'dmg': 100}]
 
 
 player = Person(460, 65, 60, 34, magic)
-print(player.generate_spell_damage(0))
-print(player.generate_spell_damage(1))
+enemy = Person(1200, 65, 45, 25, magic)
+
+running = True
+i = 0
+
+print(bcolors.FAIL + bcolors.BOLD + "AN ENEMY ATTACKS!" + bcolors.ENDC)
+
+while running:
+    print('======================')
+    player.choose_action()
+    choice = input('Choose Action:')
+    index = int(choice) - 1
+    print('You chose', choice)
+
+    if index == 0:
+        dmg = player.generate_damage()
+        enemy.take_damage(dmg)
+        print('You attacked for', dmg, 'points of damage')
+    elif index == 1:
+        player.choose_magic()
+        magic_choice = int(input('Choose Magic:')) - 1
+        magic_dmg = player.generate_spell_damage(magic_choice)
+        spell = player.get_spell_name(magic_choice)
+        cost = player.get_spell_mp_cost(magic_choice)
+
+        current_mp = player.get_mp()
+
+        if cost > current_mp:
+            print(bcolors.FAIL + '\nNOT ENOUGH MP!\n' + bcolors.ENDC)
+            continue
+
+        player.reduce_mp(cost)
+        enemy.take_damage(magic_dmg)
+        print(bcolors.OKBLUE + '\n' + spell + 'deals', str(magic_dmg), 'points of damage' + bcolors.ENDC)
+
+
+    enemy_choice = 1
+
+    enemy_dmg = enemy.generate_damage()
+    player.take_damage(enemy_dmg)
+    print('Enemy attacks for', enemy_dmg)
+
+    print('----------------------')
+    print('Enemy HP:', bcolors.FAIL + str(enemy.get_hp()) + '/' + str(enemy.get_max_hp()) + bcolors.ENDC + '\n')
+
+    print('Your HP:', bcolors.OKGREEN + str(player.get_hp()) + '/' + str(player.get_max_hp()) + bcolors.ENDC)
+    print('Your MP:', bcolors.OKBLUE + str(player.get_mp()) + '/' + str(player.get_max_mp()) + bcolors.ENDC + '\n')
+
+    if enemy.get_hp() == 0:
+        print(bcolors.OKGREEN + 'YOU WIN!' + bcolors.ENDC)
+        running = False
+    elif player.get_hp() == 0:
+        print(bcolors.FAIL + 'YOUR ENEMY HAS DEFEATED YOU!' + bcolors.ENDC)
+        running = False
+
+
 
 
